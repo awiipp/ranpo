7e66b66f5babe435da09738f2c6e5a6c25a7c7de
awiipp <sayidariipp@gmail.com>
2026-04-26 18:38:39 +0700

    feat: improve http code styling in json response header

--- internal/renderer/style.go
+++ internal/renderer/style.go
@@ -10,9 +10,9 @@ var (
 	nullStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("241")) // dim
 	punctStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("250")) // light
 
-	statusOKStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("114"))
-	statusWarnStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("215"))
-	statusErrStyle  = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("204"))
+	statusOKStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("114")).Padding(0, 1)
+	statusWarnStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("215")).Padding(0, 1)
+	statusErrStyle  = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("204")).Padding(0, 1)
 	dimStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
 	labelStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("75")).Bold(true)
 )
